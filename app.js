19e0b1d1394f2ca4d28244fe1d8312e5ae5c8c17
JASON <nj.ho@hotmail.com>
2017-08-17 12:47:09 -0600

    new commit

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ console.log(conf.FB_SERVICEACCOUNT);
 
 function initFlashlight() {
   console.log('Connecting to Firebase %s'.grey, conf.FB_URL);
+  console.log(conf.FB_SERVICEACCOUNT);
   fbutil.init(conf.FB_URL, conf.FB_SERVICEACCOUNT);
   PathMonitor.process(esc, conf.paths, conf.FB_PATH);
   SearchQueue.init(esc, conf.FB_REQ, conf.FB_RES, conf.CLEANUP_INTERVAL);
